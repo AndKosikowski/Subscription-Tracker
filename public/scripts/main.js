414f421aea10f0764656a154ef17b59c6141a190
agentthatguy <kosikoaj@rose-hulman.edu>
2021-08-03 14:38:35 -0400

    messing with calendar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,65 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
+/* @author 
+ * Andrew Kosikowski and Athena Henderson
  */
 
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
 rhit.functionName = function () {
-	/** function body */
+
 };
 
-rhit.ClassName = class {
+rhit.MainPageController = class {
 	constructor() {
 
 	}
 
-	methodName() {
+}
+
+rhit.SubscriptionPageController = class {
+	constructor() {
 
 	}
 }
 
-/* Main */
-/** function and class syntax examples */
+rhit.AccountPageController = class {
+	constructor() {
+
+	}
+}
+
+rhit.AuthController = class {
+	constructor() {
+
+	}
+}
+
+rhit.CalendarCreator = function() {
+	document.addEventListener('DOMContentLoaded', function() {
+		var calendarEl = document.getElementById('calendar');
+		var calendar = new FullCalendar.Calendar(calendarEl, {
+		  initialView: 'dayGridMonth',
+		  navLinks: true,
+		  navLinkDayClick: function() {
+			window.location.href = "/manage.html"
+		  }
+		});
+		calendar.render();
+	  });
+}
+
 rhit.main = function () {
 	console.log("Ready");
+	rhit.CalendarCreator();
+	calendar.addEvent(calendarEl, {
+		events: [
+			{title: 'Netflix',
+			allDay: true,
+			start: '2021-08-03',
+			end: '2021-08-03'}
+		]
+	});
 };
+
+
 
 rhit.main();
